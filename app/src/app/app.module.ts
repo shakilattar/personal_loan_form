f7d74646187c9332afee7c04a0616492ac10131f
Shakil Attar <attarshakil38@gmail.com>
2021-05-08 15:12:06 +0530

    created angular app

---
+++ app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RfsComponent } from './rfs/rfs.component' 
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RfsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ app/src/app/rfs/rfs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rfs',
+  templateUrl: './rfs.component.html',
+  styleUrls: ['./rfs.component.css']
+})
+export class RfsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
