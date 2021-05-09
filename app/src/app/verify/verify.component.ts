1ae797a54c85b1f73ff3d408ed48d1eec1f4e392
Shakil Attar <attarshakil38@gmail.com>
2021-05-09 13:50:58 +0530

    form improved

--- app/src/app/verify/verify.component.ts
+++ app/src/app/verify/verify.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-verify',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./verify.component.css']
 })
 export class VerifyComponent implements OnInit {
-
-  constructor() { }
+  regForm: FormGroup;
+  //submitted: false;
+  public newData:any;
+  submitted: boolean;
+  
+  constructor(private formbuilder:FormBuilder) { }
 
   ngOnInit(): void {
+    this.regForm = this.formbuilder.group({
+      city:['', Validators.required],
+      panNumber:['', [Validators.required, Validators.maxLength(10)]],
+      fullName:['', [Validators.required, Validators.maxLength(140)]],
+      email:['', [Validators.required, Validators.email, Validators.maxLength(255)]],
+      mobileNumber:['', [Validators.required, Validators.minLength(10)]],
+      otp:['', [Validators.required, Validators.minLength(4)]]
+    });
   }
 
+  get formData(){ return this.regForm.controls; }
+
+  onSubmit(){
+    this.submitted = true;
+
+    if(this.regForm.invalid){
+      return;
+    }
+    alert(JSON.stringify(this.regForm.value));
+
+  }  
 }
